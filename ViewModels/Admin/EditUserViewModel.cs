9f0684e2075ad9cc93b155ec62c7340d937254c3
Anton Dehtiar <anton.dehtiar@gmail.com>
2024-03-31 23:32:43 +0300

    adding a new design

--- ViewModels/Admin/EditUserViewModel.cs
+++ ViewModels/Admin/EditUserViewModel.cs
@@ -12,6 +12,7 @@ namespace SwiftTicketApp.ViewModels.Admin
         public string Email { get; set; } = string.Empty;
         [Required(ErrorMessage = "Username is required.")] 
         public string UserName { get; set; } = string.Empty;
+
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "New Password")]
